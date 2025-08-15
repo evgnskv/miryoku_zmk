4f5f1fc30cb8cb6b8154fc0928c4d79aef103b20
Evgeniy Noskov <evgnskv@pm.me>
2025-08-16 00:30:16 +0900

    rollback BUTTON rm

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -5,7 +5,7 @@
 #define MIRYOKU_CLIPBOARD_MAC
 #define U_TAPPING_TERM 100
 
-#define MIRYOKU_ALTERNATIVES_BASE_QWERTY \
+#define MIRYOKU_LAYER_BASE \
 &kp Q,             &kp W,             &kp E,             &kp R,             &kp T,             &kp Y,             &kp U,             &kp I,             &kp O,             &kp P,             \
 U_MT(LGUI, A),     U_MT(LALT, S),     U_MT(LCTRL, D),    U_MT(LSHFT, F),    &kp G,             &kp H,             U_MT(LSHFT, J),    U_MT(LCTRL, K),    U_MT(LALT, L),     U_MT(LGUI, SQT),   \
 U_LT(RGUI, Z),     U_MT(RALT, X),     &kp C,             &kp V,             &kp B,             &kp N,             &kp M,             &kp COMMA,         U_MT(RALT, DOT),   U_LT(RGUI, SLASH), \
@@ -57,6 +57,7 @@ U_NP,              U_NP,              &kp LALT,          &kp SPACE,         U_NA
 MIRYOKU_X(BASE,   "Base")    \
 MIRYOKU_X(EXTRA,  "Extra")   \
 MIRYOKU_X(TAP,    "Tap")     \
+MIRYOKU_X(BUTTON, "Button")  \
 MIRYOKU_X(NAV,    "Nav")     \
 MIRYOKU_X(MOUSE,  "Mouse")   \
 MIRYOKU_X(MEDIA,  "Media")   \
@@ -76,13 +77,14 @@ MIRYOKU_X(DB,     "D Extra")
 #define U_BASE   0
 #define U_EXTRA  1
 #define U_TAP    2
-#define U_NAV    3
-#define U_MOUSE  4
-#define U_MEDIA  5
-#define U_NUM    6
-#define U_SYM    7
-#define U_FUN    8
-#define U_CA     9
-#define U_CB    10
-#define U_DA    11
-#define U_DB    12
+#define U_BUTTON 3
+#define U_NAV    4
+#define U_MOUSE  5
+#define U_MEDIA  6
+#define U_NUM    7
+#define U_SYM    8
+#define U_FUN    9
+#define U_CA    10
+#define U_CB    11
+#define U_DA    12
+#define U_DB    13
